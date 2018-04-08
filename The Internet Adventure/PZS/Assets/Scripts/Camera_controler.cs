a64d07aa5150cca4e78c889a6521a1b5ba45c2ff
Pitrex <226129@student.pwr.edu.pl>
2018-04-08 21:10:25 +0200

    Dodałem ruch kamerą oraz killzone porusza się razem z nią

---
+++ The Internet Adventure/PZS/Assets/Scripts/Camera_controler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_controler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    const float speed = 1.0f;
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime * speed,transform.position.z);
+	}
+}
