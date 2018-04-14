18319171bd2c918e271da5b66558c3ef56b4fa13
KrzysiekAgi <225970@student.pwr.edu.pl>
2018-04-14 16:03:50 +0200

    PauseMenu
    Works on test_scene. On Level1 buttons don't work (no idea why :/ )

---
+++ The Internet Adventure/PZS/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool paused = false;
+    public GameObject pauseMenuUI;
+    private string sceneName = "MainMenu";
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; 
+        paused = false;
+    }
+
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; //freeze
+        paused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
