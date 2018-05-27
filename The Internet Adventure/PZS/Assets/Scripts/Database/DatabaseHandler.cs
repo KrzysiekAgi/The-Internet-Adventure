c69c9303d86c084103267fe67db519b21adafc8f
Krzysztof Sobocinski <kszys96@outlook.com>
2018-05-27 21:08:20 +0200

    Login Menu

--- The Internet Adventure/PZS/Assets/Scripts/Database/DatabaseHandler.cs
+++ The Internet Adventure/PZS/Assets/Scripts/Database/DatabaseHandler.cs
@@ -7,6 +7,7 @@ using System.Text;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DatabaseHandler : MonoBehaviour {
 
@@ -35,15 +36,13 @@ public class DatabaseHandler : MonoBehaviour {
             connectionString += "false;";
         }*/
 
-        try
-        {
-            con = new MySqlConnection(connectionString);
-            con.Open();
-            Debug.Log("Mysql State: " + con.State);
-        } catch(Exception e)
-        {
-            Debug.Log(e);
-        }
+
+
+        //Register("Pitrex","kappa", "pitrex111@interia.pl", "M", "PITREX");
+        //Login("Pitreax", "kappa");
+        //UpdateLastGame("10000", "5.5", "200", "0", "KRzych");
+
+        //SceneManager.LoadScene("login_menu");
 
     }
 
@@ -65,4 +64,118 @@ public class DatabaseHandler : MonoBehaviour {
         return con.State.ToString();
     }
 
+    public bool Login(string login, string password)
+    {
+        Connect();
+        string loginString = "Select count(*) from USER where login like '" + login + "' and password like '" + password + "';";
+        MySqlCommand cmdLogin = con.CreateCommand();
+        cmdLogin.CommandType = CommandType.Text;
+        cmdLogin.CommandText = loginString;
+        if (Convert.ToInt32(cmdLogin.ExecuteScalar()) == 1)
+        {
+            Debug.Log("Logged in");
+            con.Close();
+            return true;
+        }
+        Debug.Log("Wrong login or password");
+        con.Close();
+        return false;
+
+    }
+
+    public bool Register(string login,string password, string email, string sex, string nick)
+    {
+        Connect();
+        int cos = 0;
+        string checkString = "Select count(*) from USER where login like '"+login+"' or email like '"+email+"' or nick like '"+nick+"';";
+        //string checkString = "Select count(*) from USER where false;";
+        MySqlCommand cmdCheck = con.CreateCommand();
+        cmdCheck.CommandType = CommandType.Text;
+        cmdCheck.CommandText = checkString;
+        cos = Convert.ToInt32(cmdCheck.ExecuteScalar());
+
+        if (cos != 0)
+        {
+            con.Close();
+            return false; // TODO zamien cos na madra nazwe i dopisz dlaczego false
+        }
+        //cmd.ExecuteNonQuery();
+        //DataTable dt = new DataTable();
+        //MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+        //adapter.Fill(dt);
+        //cos = Convert.ToInt32(dt.);
+
+        string creatingString = "Insert into USER Values( null, '" + login + "', '" + password + "', '" + email + "', null, 1, '" + sex + "', '" + nick + "');"; //TODO dopisz numer przy kolejnych takich samych nickach
+        MySqlCommand cmdCreate = con.CreateCommand();
+        cmdCreate.CommandType = CommandType.Text;
+        cmdCreate.CommandText = creatingString;
+        if(cmdCreate.ExecuteNonQuery()==1) Debug.Log("Created account: " + login);
+
+        Debug.Log("cos: " + cos);
+
+        con.Close();
+
+        return true;
+    }
+
+    public void UpdateLastGame (string score, string time, string gold, string number_of_memes, string login)
+    {
+        Connect();
+        string statisticString = "Update LAST_GAME L, USER U Set L.score ="+score+", L.time="+time+",L.gold ="+gold+", L.number_of_mems=0 where L.user_id = U.id and U.login like '"+login+"';";
+        MySqlCommand cmdUpdate = con.CreateCommand();
+        cmdUpdate.CommandType = CommandType.Text;
+        cmdUpdate.CommandText = statisticString;
+        cmdUpdate.ExecuteNonQuery();
+
+        con.Close();
+    }
+
+
+    public string[][] ViewRanking ()
+    {
+        Connect();
+        string rankingString = "Select * from RANKING;";
+        MySqlDataReader reader;
+        MySqlCommand cmdRank = con.CreateCommand();
+        cmdRank.CommandType = CommandType.Text;
+        cmdRank.CommandText = rankingString;
+        reader = cmdRank.ExecuteReader();
+        try
+        {
+            while (reader.Read())
+            {
+                Debug.Log(reader.GetString(0));
+            }
+        }
+        finally { }
+        
+        reader.Close();
+        con.Close();
+    
+
+        return null;
+    }
+
+    public void DeleteAccount(string login)
+    {
+        Connect();
+        string deleteString = "Delete from USER where login like '" +login+"';";
+        MySqlCommand cmdDelete = con.CreateCommand();
+        cmdDelete.CommandType = CommandType.Text;
+        if (cmdDelete.ExecuteNonQuery() == 1) Debug.Log("Deleted account: " + login);
+        con.Close();
+    }
+
+    private void Connect()
+    {
+        try
+        {
+            con = new MySqlConnection(connectionString);
+            con.Open();
+            Debug.Log("Mysql State: " + con.State);
+        } catch(Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
 }
